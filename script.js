2cf4e4de92eeaa252f2224de35986d73f1612be6
sriramulagopi <129825285+sriramulagopi@users.noreply.github.com>
2024-01-24 11:29:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let first = document.getElementById("name1");
+let last = document.getElementById("name2");
+let number = document.getElementById("number");
+let mail =  document.getElementById("mail");
+let submit = document.getElementById("submit");
+submit.addEventListener("click",(e)=>{
+    e.preventDefault();
+    console.log("First Name:"+first.value);
+    console.log("Last Name:"+last.value);
+    console.log("Phone Number:"+number.value);
+    console.log("Email ID:"+mail.value);
+})
